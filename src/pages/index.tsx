e25f62fff58244717531287b666ab53f806cd975
Eduardo Urbina Dev <eduardoh.urbina.developer@gmail.com>
2020-05-24 16:55:31 -0500

    added image and modified title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ const BlogIndex = ({ data, location }: PageProps<Data>) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
+      <SEO title="Urbina JS" />
       <Bio />
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
